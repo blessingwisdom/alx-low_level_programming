81e693d3cee138ef719ccf64350a0cc6f7156933
Blessing <prodigeebw@gmail.com>
2022-09-12 19:02:47 -0700

    Low level programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	int s = strlen ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" s);
 	return (1);
 }
