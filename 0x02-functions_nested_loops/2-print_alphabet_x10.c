01d7b052dfa670873f3d67aa4ab53748e2f11a4e
Blessing <prodigeebw@gmail.com>
2022-09-14 21:21:43 -0700

    Functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet 10 times
+ * print_alphabet_x10 - print alphabet 10 times
  *
  * Return: void
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int timer;
 
