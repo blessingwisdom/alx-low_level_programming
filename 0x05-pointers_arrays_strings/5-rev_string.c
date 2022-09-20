07b0b7ba2207bdb4a361777a4bb408df23b978ca
Blessing <prodigeebw@gmail.com>
2022-09-19 19:28:22 -0700

    Adding task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ * @s: a string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j, l;
+	char c;
+
+	for (i = 0; s[i] != '\0'; i++);
+	l = i;
+	for (i--, j = 0; j < 1/2; i--, j++)
+	{
+		c = s[j];
+		s[j] = s[i];
+		s[i] = c;
+	}
+}
