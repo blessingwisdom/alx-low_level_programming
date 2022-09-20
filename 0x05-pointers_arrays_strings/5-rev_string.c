30f0be02ee76f363934b6aa6d4f8663f347c20ba
Blessing <prodigeebw@gmail.com>
2022-09-19 19:33:27 -0700

    Correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,10 @@ void rev_string(char *s)
 	int i, j, l;
 	char c;
 
-	for (i = 0; s[i] != '\0'; i++);
+	for (i = 0; s[i] != '\0'; i++)
+		;
 	l = i;
-	for (i--, j = 0; j < l/2; i--, j++)
+	for (i--, j = 0; j < l / 2; i--, j++)
 	{
 		c = s[j];
 		s[j] = s[i];
