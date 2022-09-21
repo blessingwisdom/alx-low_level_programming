1c37ecb4e34674935fe8e4aab3ea1fd3d9cd9de9
Blessing <prodigeebw@gmail.com>
2022-09-21 08:31:43 -0700

    Task 0 done and README

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: character to be checked
+ * @src: character to be checked
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		m++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[m] = '\0';
+
+	return (dest);
+}
