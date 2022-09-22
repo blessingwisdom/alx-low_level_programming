fa076bceee1740c35c29636e9af993f178d03003
Blessing <prodigeebw@gmail.com>
2022-09-21 19:59:19 -0700

    Adding task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,11 +11,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int m;
 
-	for (m = 0; m < n && src[m] != '\0'; m++)
+	m = 0;
+	while (m < n && src[m] != '\0')
 		dest[m] = src[m];
+		m++;
 
-	for ( n < m; m++)
+	while (m < n)
 		dest[m] = '\0';
+		m++;
 	
 	return (dest);
 }
