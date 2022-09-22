8cf1877a3b2e0ce86fcd35aef74471771ce47734
Blessing <prodigeebw@gmail.com>
2022-09-21 19:44:18 -0700

    TAsk 0 added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,14 +12,14 @@ char *_strcat(char *dest, char *src)
 	int j = 0;
 
 	while (dest[i] != '\0')
-		m++;
+		i++;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
-	dest[m] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
