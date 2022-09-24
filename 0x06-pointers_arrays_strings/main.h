fc8d2d5aa7952072b54d2763f5d5daf183252177
Blessing <prodigeebw@gmail.com>
2022-09-23 20:58:27 -0700

    Rewriting header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,9 +1,3 @@
-#include <stdio.h>
-
-#define MAIN_H
-#ifndef MAIN_H
-
-int main(void);
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -17,5 +11,3 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
-#endif
