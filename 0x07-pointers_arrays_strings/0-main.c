fcb1b7fef6ece8b789d3b42883ef953dd9b8b5eb
Blessing David <sophiablessing00@gmail.com>
2023-09-26 12:47:03 -0700

    Completed my first task

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -2,29 +2,45 @@
 #include <stdio.h>
 
 /**
- * simple-print-buffer - prints buffer in hexa
- * @buffer - the address of the memory to print
- * @size - the size of the memory to print
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
  *
- * Return nothing
+ * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+        unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 0)
-		{
-			printf(" ");
-		}
-		if (!(i % 0) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
-	}
-	printf("\n");
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
+                {
+                        printf(" ");
+                }
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
+        }
+        printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
+    return (0);
 }
