a913e2deb7d1744023094b39ac105da54f52f51e
Blessing David <prodigeebw@gmail.com>
2023-09-26 16:14:08 -0700

    Task done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,24 +1,34 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * *cap_string - capitalizes all words of a string.
- * @s: string.
- * Return: string.
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
 char *cap_string(char *s)
 {
-	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	int a = 0;
-	unsigned long int i;
-	
-	for (a = 0; s[a] != '\0'; a++)
+	int i, j;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (i = 0; i < strlen(spc); i++)
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
 		{
-			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
-				s[a] = s[a] - 32;
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
 		}
 	}
+
 	return (s);
 }
